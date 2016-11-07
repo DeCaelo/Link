cc003138a9bcfedf8d4b9833e47c6e136de07240
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-07 15:54:07 +0100

    remove title column from posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107124554) do
+ActiveRecord::Schema.define(version: 20161107144522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
