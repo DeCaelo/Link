04b86570fa37adf93ed001f2ff8f6db606824728
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-11-15 08:59:27 +0100

    users controller, profile & edit views & routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :profile, :edit ,:update, :destroy ]
+
+  def profile
+  end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(current_user)
+    if @user.update_attributes(user_params)
+      redirect_to profile_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    if @user.destroy
+      redirect_to root_url, notice: "User deleted."
+    else
+      render 'profile'
+    end
+  end
+
+  private
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+   params.require(:user).permit(:email, :avatar)
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :posts
+  get '/profile' => 'users#profile'
+  resources :users, only: [ :edit, :update, :destroy ]
 end
